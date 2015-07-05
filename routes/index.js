46fe89d26b151e622aae67f7ab84f6a53d3d4435
Will Humphreys <whumphreys@gmail.com>
2015-07-05 21:09:29 +0100

    Tidyup names

--- routes/index.js
+++ routes/index.js
@@ -3,21 +3,21 @@ var router = express.Router();
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
 });
 
 /* GET Userlist page. */
-router.get('/userlist', function(req, res) {
-  var db = req.db;
-  var collection = db.get('scripts');
-  collection.find({},{ limit : 10},function(e,docs){
-    //console.log(e)
-    console.log(docs)
-    res.render('userlist', {
-      "results" : docs
+router.get('/scripts', function (req, res) {
+    var db = req.db;
+    var collection = db.get('scripts');
+    collection.find({}, {limit: 10}, function (e, docs) {
+        //console.log(e)
+        console.log(docs)
+        res.render('scripts', {
+            "results": docs
+        });
     });
-  });
 
 
 });
