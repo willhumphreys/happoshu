93e55c0966a599a6d4ea46993f470153071c3a29
Will Humphreys <whumphreys@gmail.com>
2015-07-05 18:25:06 +0100

    Simple mongo

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/* GET Userlist page. */
+router.get('/userlist', function(req, res) {
+  var db = req.db;
+  var collection = db.get('scripts');
+  collection.findOne({},{},function(e,docs){
+    //console.log(e)
+    console.log(docs)
+    res.render('userlist', {
+      "results" : docs
+    });
+  });
+
+
+});
+
+module.exports = router;
