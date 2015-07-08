f643de1cad2f0936b98f6ecb114ee60e361e5214
Will Humphreys <will.humphreys@bbc.co.uk>
2015-07-08 13:30:32 +0100

    Fix pulling files link.

--- routes/scenariofiles.js
+++ routes/scenariofiles.js
@@ -2,17 +2,35 @@ var express = require('express');
 var AWS = require('aws-sdk');
 var router = express.Router();
 
+
+String.prototype.endsWith = function (suffix) {
+    return this.indexOf(suffix, this.length - suffix.length) !== -1;
+};
+
 router.get('/', function (req, res, next) {
 
-    var bucketSegment = req.originalUrl.substr(req.originalUrl.lastIndexOf('/') + 1);
-    console.log(bucketSegment);
+    console.log('req is: ' + req);
+
+
+    console.log(req.originalUrl)
+    ///scenariofiles/spx_1minute/
+
+    var noTrailingSlash = req.originalUrl;
+    if (req.originalUrl.endsWith('/')) {
+        noTrailingSlash = noTrailingSlash.substring(0, noTrailingSlash.length - 1);
+    }
+
+    console.log('noTrailing is ' + noTrailingSlash);
+
+    var bucketSegment = req.originalUrl.substr(noTrailingSlash.lastIndexOf('/') + 1);
+    console.log('bucket segment is: ' + bucketSegment);
 
     var s3 = new AWS.S3();
 
 
     function handleMatchedBucket() {
         var params = {
-            Bucket: 'livedata-matcha', /* required */
+            Bucket: 'livedata-matcha',
             Prefix: '' + bucketSegment
 
 
