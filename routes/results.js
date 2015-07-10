f158c466ce9f532afc97ff02b09db2477002eec1
Will Humphreys <whumphreys@gmail.com>
2015-07-10 23:03:59 +0100

    Get results working on the backend

--- routes/results.js
+++ routes/results.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var AWS = require('aws-sdk');
+var underscore = require('underscore')
 var router = express.Router();
 
 router.get('/', function (req, res, next) {
 
     function handleMatchedBucket(prefix, s3) {
+
+        var allResultsJson = [];
 
         var params = {
             Bucket: 'livedata-matcha', /* required */
@@ -18,21 +21,41 @@ router.get('/', function (req, res, next) {
                 res.send('broken');
             } // an error occurred
             else {
+
+                var finished = underscore.after(data.Contents.length, doResponse);
+
                 console.log(data);
 
                 console.log(data.Contents)
 
+
                 for (var i = 0; i < data.Contents.length; i++) {
                     console.log(data.Contents[i].Key);
 
-                    var params = {Bucket: 'livedata-matcha', Key: data.Contents[i].Key};
-                    var whatWeGot = s3.getObject(params);
-                    console.log(whatWeGot);
+                    //  var params = {Bucket: 'livedata-matcha', Key: data.Contents[i].Key};
+                    s3.getObject({
+                        Bucket: 'livedata-matcha',
+                        Key: data.Contents[i].Key
+                    }).on('success', function (response) {
+                        console.log("Key was", response.request.params.Key);
+                        // var resultsJson = response.Body.toString()
+                        var resultsJson = JSON.parse(response.data.Body.toString());
+                        //  console.log(resultsJson);
+                        allResultsJson = allResultsJson.concat(resultsJson);
+
+                        console.log(allResultsJson);
+                        finished();
+                    }).send();
+
 
                 }
 
-                res.json(data);
-            }           // successful response
+
+            }
+
+            function doResponse() {
+                res.json(allResultsJson);
+            }// successful response
         });
 
     }
