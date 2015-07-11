7838651d241c3b10e16490c42048606ff13468b3
Will Humphreys <whumphreys@gmail.com>
2015-07-11 19:33:38 +0100

    Add the missing files

---
+++ client/app/main/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('happoshuApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
