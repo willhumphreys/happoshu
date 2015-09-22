6d8954671babe37dc0028faa25f34e0559e259a1
Will Humphreys <whumphreys@gmail.com>
2015-09-22 19:08:27 +0100

    Add entry to index page.

--- client/app/mergedPosition/mergedPosition.controller.js
+++ client/app/mergedPosition/mergedPosition.controller.js
@@ -1,15 +1,67 @@
 'use strict';
 
 angular.module('happoshuApp')
-    .controller('MergedPositionCtrl', function ($scope, MergedPositionService) {
+    .controller('MergedPositionCtrl', function ($scope, MergedPositionService, uiGridConstants) {
+        $scope.columns =
+            [
+                {field: 'runName'},
+                {field: 'name', width: '5%', maxWidth: 200, minWidth: 100},
+                {field: 'Profit', type: 'number', width: '5%', maxWidth: 200, minWidth: 90},
+
+                // multiple filters
+                {
+                    field: 'Winners', type: 'number', width: '5%', maxWidth: 200, minWidth: 90, filters: [
+                    {
+                        condition: uiGridConstants.filter.GREATER_THAN,
+                        placeholder: 'greater than'
+                    },
+                    {
+                        condition: uiGridConstants.filter.LESS_THAN,
+                        placeholder: 'less than'
+                    }]
+
+                },
+                {field: 'Losers', type: 'number', width: '5%', maxWidth: 200, minWidth: 90},
+                {
+                    field: 'ProfitPerTrade', type: 'number', width: '5%', maxWidth: 250, minWidth: 190,
+                    sort: {
+                        direction: uiGridConstants.DESC,
+                        priority: 1
+                    }
+
+                },
+                {field: 'Stop', type: 'number', width: '5%', maxWidth: 200, minWidth: 90},
+                {field: 'TrailingStop', type: 'number', width: '5%', maxWidth: 200, minWidth: 90},
+                {
+                    field: 'OpenTickOffset',
+                    type: 'number',
+                    width: '5%',
+                    maxWidth: 200,
+                    minWidth: 90,
+                    displayName: 'Offset'
+                },
+                {field: 'Time', width: '5%', maxWidth: 200, minWidth: 90},
+                {
+                    field: 'WinnerLoserRatio',
+                    type: 'number',
+                    width: '5%',
+                    maxWidth: 200,
+                    minWidth: 90,
+                    displayName: 'WLR'
+                },
+
+                {field: 'DayOfWeek', width: '5%', maxWidth: 200, minWidth: 90, displayName: 'DOW'}
+
+            ];
 
         $scope.gridOptions2 = {
             enableColumnResizing: true,
             enableSorting: true,
-            enableFiltering: true//,
-            //  columnDefs: $scope.columns
+            enableFiltering: true,
+            columnDefs: $scope.columns
 
         };
+
 
         MergedPositionService.getMergedPositions().then(function (dataResponse) {
 
