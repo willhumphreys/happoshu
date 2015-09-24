4b1a9008963b0c9aee5f118163fa676fad929478
Will Humphreys <will.humphreys@bbc.co.uk>
2015-09-24 16:52:24 +0100

    Add contract to frontend

--- client/app/mergedPosition/mergedPosition.service.js
+++ client/app/mergedPosition/mergedPosition.service.js
@@ -3,10 +3,16 @@
 angular.module('happoshuApp')
     .service('MergedPositionService', function ($http) {
         this.getMergedPositions = function (searchOptions) {
-            console.log('Getting mergedPositions' + searchOptions);
+            console.log('Getting mergedPositions' + searchOptions.contract);
+            var mergedPositionUrl = 'http://localhost:9000/api/mergedPositions';
+
+            if (searchOptions.contract != null) {
+                mergedPositionUrl = mergedPositionUrl + '?contract=' + searchOptions.contract;
+            }
+
             return $http({
                 method: 'GET',
-                url: 'http://localhost:9000/api/mergedPositions'
+                url: mergedPositionUrl
             });
         }
     });
