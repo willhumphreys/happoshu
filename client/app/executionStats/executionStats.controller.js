7df5e7f9d7b0d9b6446a5c1915d168d6f1c8643e
Will Humphreys <will.humphreys@bbc.co.uk>
2015-10-30 17:04:39 +0000

    Add columns for the executionStats.

--- client/app/executionStats/executionStats.controller.js
+++ client/app/executionStats/executionStats.controller.js
@@ -4,19 +4,25 @@ angular.module('happoshuApp')
     .controller('ExecutionStatsCtrl', function ($scope, ExecutionStatsService) {
         $scope.message = 'Hello';
 
-        //$scope.columns =
-        //    [
-        //        {field: 'runName'},
-        //        {field: 'description', width: '5%', maxWidth: 200, minWidth: 100},
-        //
-        //
-        //    ];
+        $scope.columns =
+            [
+                {field: '_id', with: '5%', maxWidth: 200, minWidth: 100},
+                {field: 'stage', width: '5%', maxWidth: 200, minWidth: 100},
+                {field: 'runName', width: '40%', maxWidth: 500, minWidth: 100},
+                {field: 'open', width: '5%', maxWidth: 250, minWidth: 150},
+                {field: 'close', width: '5%', maxWidth: 250, minWidth: 150},
+                {field: 'durationInSeconds', width: '5%', maxWidth: 200, minWidth: 100},
+                {field: 'durationInMillis', width: '5%', maxWidth: 200, minWidth: 100},
+                {field: 'created', width: '5%', maxWidth: 200, minWidth: 100}
+
+
+            ];
 
         $scope.gridOptions2 = {
             enableColumnResizing: true,
             enableSorting: true,
-            enableFiltering: true//,
-            //  columnDefs: $scope.columns
+            enableFiltering: true,
+            columnDefs: $scope.columns
 
         };
 
