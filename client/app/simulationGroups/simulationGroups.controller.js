b94bfb8ae5ae7654a59bba659ecc0d43a864cd67
Will Humphreys <will.humphreys@bbc.co.uk>
2015-11-11 11:36:23 +0000

    tidyup

--- client/app/simulationGroups/simulationGroups.controller.js
+++ client/app/simulationGroups/simulationGroups.controller.js
@@ -3,29 +3,29 @@
 angular.module('happoshuApp')
     .controller('SimulationGroupsCtrl', function ($scope, SimulationGroupsService) {
 
-      $scope.columns =
-          [
-            {field: 'runName'},
-              {field: 'description', width: '40%', maxWidth: 600, minWidth: 100},
-              {field: 'dirty'}
+        $scope.columns =
+            [
+                {field: 'runName'},
+                {field: 'description', width: '40%', maxWidth: 600, minWidth: 100},
+                {field: 'dirty'}
 
 
-          ];
+            ];
 
-      $scope.gridOptions2 = {
-        enableColumnResizing: true,
-        enableSorting: true,
-        enableFiltering: true,
-        columnDefs: $scope.columns
+        $scope.gridOptions2 = {
+            enableColumnResizing: true,
+            enableSorting: true,
+            enableFiltering: true,
+            columnDefs: $scope.columns
 
-      };
+        };
 
-      SimulationGroupsService.getSimulationGroups().then(function (dataResponse) {
+        SimulationGroupsService.getSimulationGroups().then(function (dataResponse) {
 
-        console.log('hello');
-        //console.log(dataResponse);
-        console.log('endhello');
-          // $scope.results2 = dataResponse;
-        $scope.gridOptions2.data = dataResponse.data;
-      });
+            console.log('hello');
+            //console.log(dataResponse);
+            console.log('endhello');
+            // $scope.results2 = dataResponse;
+            $scope.gridOptions2.data = dataResponse.data;
+        });
     });
